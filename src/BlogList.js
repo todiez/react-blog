89b2da9458a96ec50814778b22c1f551a9cef03f
todiez <tobias@diez.cc>
2024-09-24 17:25:27 +0200

    episode 13 done (functions as props)

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,8 +1,8 @@
-const BlogList = (props) => {
+const BlogList = ({ blogs, title, handleDelete }) => {
   // props object, passed down blogs are inside this object
-
-  const blogs = props.blogs;
-  const title = props.title;
+  //and taken out of the object like the following or as a destructered object as done above
+  // const blogs = props.blogs;
+  // const title = props.title;
 
   //props in practice, just destructure and change the arrow function to
   // const BlogList = ({blogs, title})
@@ -14,6 +14,7 @@ const BlogList = (props) => {
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
+          <button onClick={() => handleDelete(blog.id)}>delete blog</button>
         </div>
       ))}
     </div>
--- src/Home.js
+++ src/Home.js
@@ -13,12 +13,23 @@ const Home = () => {
     { title: "My third website", body: "lorem ipsum...", author: "Ida", id: 3 },
   ]);
 
+  const handleDelete = (id) => {
+    //can be passed down as a prop as well
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title='All my blogs!'/>
+      <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete} />
       {
         //Props is used to pass down the blogs from the parent to the child component called BlogList.js
       }
+      <BlogList
+        blogs={blogs.filter((blog) => blog.author === "Dora")}
+        title="Dora's blogs!"
+        handleDelete={handleDelete}
+      />
     </div>
   );
 };
