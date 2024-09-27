a1f2a77b75bdaa4ffaf27d1549c7f16ac8d0921e
todiez <tobias@diez.cc>
2024-09-27 17:17:37 +0200

    Links added to intercept server requests

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,22 +1,27 @@
+import { Link } from "react-router-dom";
+// the Link component is used to create links to different routes and
+// implement navigation around your application without causing a full page refresh
+// further, it is intercepting clicks to handle routing internally instead of a server request
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="links">
-        <a href="/">Home</a>
-        <a
-          href="/create"
+        <Link to="/">Home</Link>
+        <Link
+          to="/create"
           style={
             //Outer curly braces: tell react that its a dynamic value
             {
               //Inner curly braces: JS Object
               color: "white",
               backgroundColor: "blue",
-              borderRadius: '18px'
+              borderRadius: "18px",
             }
           }
         >
           New Blog
-        </a>
+        </Link>
       </div>
     </nav>
   );
