5101880b945cfe2ff50f1c163a17cc665127e889
todiez <tobias@diez.cc>
2024-09-27 17:01:37 +0200

    routing added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Create from "./Create";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <BrowserRouter>
+      {/* Router gives access to all nested components inside App function */}
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            {/* Routes ensures that at any time only one route is displayed --> all routes need to be inside
+            Navbar is shown all the time, therefore outside Routes*/}
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/create" element={<Create />}></Route>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
