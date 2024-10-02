832c33b5e491baad8f2eb6fbd1ffd537a86d8721
todiez <tobias@diez.cc>
2024-10-02 21:35:57 +0200

    get request route + .env added

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
+//require dotenv and directly invoke config method on it to attach the
+//environment variable to the process object
+require("dotenv").config();
+
 const express = require("express");
+const port = process.env.PORT;
 
 //create an express app be invoke an express function
 //the app variable now holds the main application object
 const app = express();
 
-const PORT = 4000;
+//router handler, supports reacting to specific requests
+//if a request comes in the function (2. argument) will be fired
+app.get("/", (req, res) => {
+  res.json({ m: "Welcome to the App" });
+  //  res.send("Welcome to the App" );
+});
 
 //listen for requests
-app.listen(PORT, () => {
-  console.log("listening on the Port", PORT);
+app.listen(port, () => {
+  console.log("listening on Port", port);
 });
