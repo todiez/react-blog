d66cef8ada84fb4923c534b18c2e2bb4db2ed52a
todiez <tobias@diez.cc>
2024-10-02 21:13:08 +0200

    nodemon added

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
 const express = require("express");
+
+//create an express app be invoke an express function
+//the app variable now holds the main application object
+const app = express();
+
+const PORT = 4000;
+
+//listen for requests
+app.listen(PORT, () => {
+  console.log("listening on the Port", PORT);
+});
