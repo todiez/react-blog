453c40c1d71abbf036ab9d4e56e3994e24b74acf
todiez <tobias@diez.cc>
2024-10-03 11:41:07 +0200

    mongodb connection added

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@
 require("dotenv").config();
 
 const express = require("express");
-const blogRoutes = require("./routes/blogRoutes")
+const mongoose = require("mongoose");
+const blogRoutes = require("./routes/blogRoutes");
 const port = process.env.PORT;
 
 //create an express app be invoke an express function
@@ -15,21 +16,25 @@ const app = express();
 //necessary to access the request objects of the routes
 //looks if there is any body to the request coming in, if yes it is
 //parsing and attaching it to the request object
-app.use(express.json())
+app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
-
 //grabs all different routes attached to the Router instance via
 //the variable blogRoutes and defined in the blogRoutes.js file
-//first argument: blogRoutes fired only when it comes to the specific path 
-app.use("/api/blogs", blogRoutes)
+//first argument: blogRoutes fired only when it comes to the specific path
+app.use("/api/blogs", blogRoutes);
 
-
-//listen for requests
-app.listen(port, () => {
-  console.log("listening on Port", port);
-});
+//connect to data base, it's async and therefore it returns a promise
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    //listen for requests only after connection to db is etablished
+    app.listen(port, () => {
+      console.log("connected to database and listening on port:", port);
+    });
+  })
+  .catch((error) => console.log(error));
