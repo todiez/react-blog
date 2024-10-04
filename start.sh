44db0b9ebd3c196764e0cb79625d1c8cf220ac31
todiez <tobias@diez.cc>
2024-10-04 22:23:35 +0200

    start script adjusted

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-cd backend && npm start &
-cd ../frontend && npm start &
-wait
+cd backend || exit
+npm start &
+BACKEND_PID=$!
+
+cd ../frontend || { kill $BACKEND_PID; exit 1; }
+npm start
+
+# Optionally, if you need to keep this script running or handle process management:
+# wait $BACKEND_PID
