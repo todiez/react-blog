a1e9c5bb2b904f83a63a57e6145d1c2053000834
todiez <tobias@diez.cc>
2024-10-09 13:49:54 +0200

    useFetch made async explicitly

--- frontend/src/useFetch.js
+++ frontend/src/useFetch.js
@@ -13,30 +13,58 @@ const useFetch = (url) => {
     //use effect is a hook that runs a function on every render of the component, e.g. to fetch data
     //useState is a hook which rerenders the component on every state change
 
+    // Initialize AbortController for cleanup
     const abortCont = new AbortController();
 
-    fetch(url, { signal: abortCont.signal })
-      .then((res) => {
-        if (!res.ok) {
-          throw Error("could not fetch the data");
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url, { signal: abortCont.signal });
+
+        if (!response.ok) {
+          throw new Error("Could not fetch the data");
         }
-        return res.json();
-      })
-      .then((data) => {
-        setData(data);
-        setIsPending(false);
-        setError(null);
-      })
-      .catch((err) => {
-        if (err.name === "AbortError") {
-          console.log("fetch aborted");
-          console.log(err);
-        } else {
+
+        const data = await response.json();
+        if (!abortCont.signal.aborted) {
+          setData(data);
+          setIsPending(false);
+        }
+      } catch (err) {
+        if (err.name === "AbortError") console.log("Fetch aborted");
+        else {
+          console.error("Fetch Error:", err);
           setIsPending(false);
           setError(err.message);
         }
-      });
+      }
+    };
 
+    //calling the async function
+    fetchData();
+
+    // fetch(url, { signal: abortCont.signal })
+    //   .then((res) => {
+    //     if (!res.ok) {
+    //       throw Error("could not fetch the data");
+    //     }
+    //     return res.json();
+    //   })
+    //   .then((data) => {
+    //     setData(data);
+    //     setIsPending(false);
+    //     setError(null);
+    //   })
+    //   .catch((err) => {
+    //     if (err.name === "AbortError") {
+    //       console.log("fetch aborted");
+    //       console.log(err);
+    //     } else {
+    //       setIsPending(false);
+    //       setError(err.message);
+    //     }
+    //   });
+
+    // Cleanup function to abort the fetch
     return () => abortCont.abort();
   }, [url]);
 
